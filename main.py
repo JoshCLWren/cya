a1ccc7f6abfd810315fe76d96571e66ef69d525a
Josh Wren <josh.wren@life.church>
2022-09-04 11:29:47 -0500

    Add flake8

--- main.py
+++ main.py
@@ -21,16 +21,12 @@ def start():
     output.speak(text=f"Hello {character.name}!")
     output.speak(text="Let's play a choose your own adventure game!")
     random_genre = random.choice(constants.genres)
-    output.speak(
-        text=f"Enter the genre of story you want to play or Press enter for a {random_genre} story."
-    )
+    output.speak(text=f"Enter the genre of story you want to play or Press enter for a {random_genre} story.")
     genre = input()
     if len(genre) == 0:
         genre = random_genre
     random_styles = random.choice(constants.styles)
-    output.speak(
-        text=f"What is the tone of this story? Or just press enter for a {random_styles} story type."
-    )
+    output.speak(text=f"What is the tone of this story? Or just press enter for a {random_styles} story type.")
     style = input()
     if len(style) == 0:
         style = random_styles
@@ -46,9 +42,9 @@ def start():
         else:
             next_line = story.advance_story(decision=next_move, name=character.name)
             print("*" * 88)
-            print(f"You gained {len(next_line)} experience!{bcolors.ENDC}")
+            print(f"You gained {len(next_line)} experience!")
             character.level_up(len(next_line))
-            print(f"You are currently level: {int(character.level)}{bcolors.ENDC}")
+            print(f"You are currently level: {int(character.level)}")
             print("*" * 88)
             print("")
     game.end_game(character, story)
